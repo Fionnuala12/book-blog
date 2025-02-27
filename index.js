d44e5405688962b17910e107acf359fb7dd01dac
Fionnuala12 <fionnuala.wilson@btinternet.com>
2025-02-27 14:11:35 +0000

    Add middleware for finding id. Add function to render homepage and create edit.ejs

--- index.js
+++ index.js
@@ -18,9 +18,33 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Middleware to find blog ID 
+function findBlogId(req, res, next) {
+  const blogId = req.params.id; 
+  req.foundPost = blogPost.find( post => post.id == blogId);
+
+  if(!req.foundPost) {
+    return res.status(404).send("Post not found");
+  }
+
+  next();
+}
+
+let books = [];
+
+// Home page 
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
-})
+  try {
+    const result = await db.query("SELECT * FROM books"); 
+    books = result.rows; 
+    console.log(books);
+    res.render("index.ejs", {books});
+  } catch(err) {
+console.log("Error:", (err));
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
